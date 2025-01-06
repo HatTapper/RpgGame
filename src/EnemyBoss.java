799d158c2fbf5076ae905188cfc61d8787640e24
HatTapper <aidanmyles206@gmail.com>
2025-01-06 08:02:41 -0500

    created new class, this will be the framework for the final boss once completed

---
+++ src/EnemyBoss.java
@@ -0,0 +1,34 @@
+// abstract class for the final boss in the game
+
+import java.util.Random;
+
+public abstract class EnemyBoss {
+    public String name;          // enemy's name
+    public int health;           // enemy's health
+    public int maxHealth;        // enemy's max health
+    public int damage;           // the damage it deals
+    public int defense;          // the defense used in negating damage taken
+    public double criticalChance;// the chance of it landing a critical hit (2x damage)
+    public int experienceGiven;  // the experience given to the player upon defeating it
+
+    // constructor
+    public EnemyBoss(String name, int health, int damage, int defense, double criticalChance, int experienceGiven) {
+        this.name = name;
+        this.health = health;
+        this.maxHealth = health;
+        this.damage = damage;
+        this.defense = defense;
+        this.criticalChance = criticalChance;
+        this.experienceGiven = experienceGiven;
+    }
+
+    // attacks the player and deals damage
+    public abstract void attack(Player player);
+    public abstract void parry(Player player);
+
+    // takes damage depending on what's given
+    public abstract void takeDamage(int damage);
+
+    // displays the enemy's stats (and flavor text if provided in the child class)
+    public abstract void inspect();
+}
