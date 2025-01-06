99c82f396b64aab407732f784c80fc15ac623b69
HatTapper <aidanmyles206@gmail.com>
2025-01-06 08:05:14 -0500

    added new skill

---
+++ src/SkillPierce.java
@@ -0,0 +1,35 @@
+public class SkillPierce extends Skill {
+
+    public SkillPierce() {
+        super.skillEnum = SkillEnums.PIERCING;
+        super.name = "Piercing Strike";
+        super.cooldown = 5;
+    }
+
+    @Override
+    public void useSkill(Player player, Enemy enemy) {
+        System.out.println("""
+                You take a step back, an ethereal spear materializing in your hands...
+                
+                You then lunge forward, directing the point at your opponent's center mass!
+                """);
+        player.setActiveSkill(SkillEnums.PIERCING);
+        player.addSkillCooldown(skillEnum, cooldown);
+
+        // this will allow defense to be negated entirely and instead deal a static 100 dmg
+        enemy.takeDamage(100 + enemy.getDefense());
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void printDescription() {
+        System.out.println("""
+                Piercing Strike: Conjure a magical spear in your hands and lunge forward at your opponent. This will deal
+                a static 100 damage that entirely ignores defense.
+                """);
+    }
+}
