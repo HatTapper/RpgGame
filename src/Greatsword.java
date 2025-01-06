81d4f49ade6f4d4c9eb0ce1b5d067902c9a9985e
HatTapper <aidanmyles206@gmail.com>
2025-01-06 08:03:01 -0500

    added new weapon

---
+++ src/Greatsword.java
@@ -0,0 +1,10 @@
+public class Greatsword extends Weapon {
+    public Greatsword() {
+        super("Greatsword", 40, 0.4);
+    }
+
+    @Override
+    public void printAttack(Player player, Enemy enemy) {
+        System.out.println("You perform a devastating sweep attack!");
+    }
+}
